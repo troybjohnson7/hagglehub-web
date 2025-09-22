37bfce4c8b18ee4d92c736037f98552c6fa8d28b
troybjohnson7 <troy.b.johnson@gmail.com>
2025-09-22 18:42:56 -0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-// Layout wrapper (header with centered logo, login button, bell, footer, bottom nav)
 import Layout from "./Layout.jsx";
 
-// Pages (these files already exist in your project)
-import About from "./about.jsx";
+// NOTE: Capital A here:
+import About from "./About.jsx";
+
 import Dashboard from "./Dashboard.jsx";
 import AddVehicle from "./AddVehicle.jsx";
 import DealDetails from "./DealDetails.jsx";
 import Messages from "./Messages.jsx";
 import Account from "./Account.jsx";
+
+// these two are lowercase in your tree:
 import PrivacyPolicy from "./privacypolicy.jsx";
 import TermsOfService from "./termsofservice.jsx";
 
-// Helper to pass the page name into Layout (your Layout uses currentPageName)
 const withLayout = (Component, pageName) => (
   <Layout currentPageName={pageName}>
     <Component />
@@ -24,21 +25,14 @@ const withLayout = (Component, pageName) => (
 export default function Pages() {
   return (
     <Routes>
-      {/* Public landing route */}
       <Route path="/" element={withLayout(About, "About")} />
-
-      {/* App routes */}
       <Route path="/dashboard" element={withLayout(Dashboard, "Dashboard")} />
       <Route path="/add-vehicle" element={withLayout(AddVehicle, "AddVehicle")} />
       <Route path="/deal" element={withLayout(DealDetails, "DealDetails")} />
       <Route path="/messages" element={withLayout(Messages, "Messages")} />
       <Route path="/account" element={withLayout(Account, "Account")} />
-
-      {/* Legal */}
       <Route path="/privacy" element={withLayout(PrivacyPolicy, "PrivacyPolicy")} />
       <Route path="/terms" element={withLayout(TermsOfService, "TermsOfService")} />
-
-      {/* Fallback */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
